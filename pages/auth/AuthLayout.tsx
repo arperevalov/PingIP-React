9e8d71e93074bc6643329e7c755d0095e5656777
arperevalov <lovetraindriver@gmail.com>
2023-04-27 20:00:46 +0700

    feat: useForm at AuthLayout

--- pages/auth/AuthLayout.tsx
+++ pages/auth/AuthLayout.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import React, { FormEvent, useRef, useEffect } from 'react';
 import Input from '../../common/Input';
 import logo from './../../public/images/logo.svg'
+import { useForm } from 'react-hook-form';
 
 interface AuthProps {
     requestToken: CallableFunction
@@ -9,19 +10,24 @@ interface AuthProps {
 
 const AuthLayout = (props:AuthProps) => {
 
-    const loginInput = useRef<HTMLInputElement>(null);
-    const passwordInput = useRef<HTMLInputElement>(null);
+    const {register, handleSubmit} = useForm();
 
-    const formSubmit = (e:FormEvent) => {
-        e.preventDefault()
-        props.requestToken(loginInput.current.value, passwordInput.current.value)
+    const formSubmit = (formData: any) => {
+        const { login, password } = formData;
+        props.requestToken(login, password)
     }
 
     return <div className='authPage'>
     <Image src={logo} alt="logo" className='authPage__logo'/>
-    <form onSubmit={formSubmit} className="authForm" >
-        <Input reference={loginInput} placeholder=" " label="Логин" type="text" isRequired={true}/>
-        <Input reference={passwordInput} placeholder=" " label="Пароль" type="password" isRequired={true}/>
+    <form onSubmit={handleSubmit(formSubmit)} className="authForm" >
+        <label className="input">
+            <input {...register("login")} className="input__input" type="text" required={true}/>
+            <span className="input__label" >Логин</span>
+        </label>
+        <label className="input">
+            <input {...register("password")} className="input__input" type="password" required={true}/>
+            <span className="input__label" >Пароль</span>
+        </label>
         <button className='button button-super' type='submit'>Войти</button>
     </form>
     </div>
